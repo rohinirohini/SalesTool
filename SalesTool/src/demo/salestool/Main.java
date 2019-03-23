8dc183917f591b8f148692765c96b6aa01aab31b
arohi <gosavirohini1993@gmail.com>
2019-03-23 21:01:46 +0530

    f

--- SalesTool/src/demo/salestool/Main.java
+++ SalesTool/src/demo/salestool/Main.java
@@ -20,10 +20,7 @@ public class Main {
 		 System.out.println(" sudhanshu   ");
 		 
 		 System.out.println(" for backing out changes");
-		 System.out.println(" for backing out changes");
-		 
-		 System.out.println(" for backing out changes");
-		 System.out.println(" for backing out changes");
+		
 
 	}
 	
