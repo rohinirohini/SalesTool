3a37791e39106a7cf308b2224417df5f52a321ad
Rohini <gosavirohini1993@gmail.com>
2020-10-03 13:24:46 +0530

    for rebase

--- SalesTool/src/demo/salestool/Main.java
+++ SalesTool/src/demo/salestool/Main.java
@@ -15,6 +15,7 @@ public class Main {
 		// TODO Auto-generated method stub
 		
 		 System.out.println("changes in branch1 will ,urge in master");
+		 System.out.println("for rebase check");
 
 	}
 	
